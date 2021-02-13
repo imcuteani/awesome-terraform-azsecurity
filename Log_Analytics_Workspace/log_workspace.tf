27331dae25759992074650a98ac1bcec8ebfeb5c
Anindita <anindita.basak@live.com>
2021-02-13 14:29:46 +0530

    Commit_master

---
+++ Log_Analytics_Workspace/log_workspace.tf
@@ -0,0 +1,14 @@
+#Create azure resource group
+
+resource "azurerm_resource_group" "example" {
+    name = "${var.resource_group}"
+    location = "${var.resource_group_location}"
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+    name = "${var.azure_log_analytics_workspace_name}"
+    location = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+    sku = "${var.loganalytics_ws_sku}"
+    retention_in_days = 30
+}
